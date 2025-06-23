96868e991c905c5c87fc2f76f0af98a7e1998104
mrvsh879 <vladislavshatrov61@gmail.com>
2025-06-23 10:03:26 +0300

    script.js

--- script.js
+++ script.js
@@ -28,7 +28,7 @@ const products = [
     category: "crypto",
     price: 65,
     rating: 4.6,
-    logo: "data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' fill='%23663399'%3E%3Cpath d='M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm4.5 14.5l-3-3-3 3-1.5-1.5 3-3-3-3L10.5 7.5l3 3 3-3L18 9l-3 3 3 3-1.5 1.5z'/%3E%3C/svg%3E",
+    logo: "https://worldvectorlogo.com/logo/kraken-2",
     features: ["Pro Trading", "KYC Tier 4", "Margin Trading"],
     availability: "limited",
     description: "Профессиональный аккаунт Kraken с максимальным уровнем верификации"
