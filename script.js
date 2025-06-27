800d9150c2f27dc18adf0e3128a9fc80dc80c272
mrvsh879 <vladislavshatrov61@gmail.com>
2025-06-27 13:49:51 +0300

    script.js

--- script.js
+++ script.js
@@ -721,7 +721,7 @@ function renderProducts() {
       </div>
       <div class="product-top">
         <div class="logo-wrapper">
-          <img src="${product.logo}" alt="${product.name}" onerror="this.src='data:image/svg+xml,%3Csvg xmlns=\\'http://www.w3.org/2000/svg\\' viewBox=\\'0 0 24 24\\' fill=\\'%23667eea\\'%3E%3Cpath d=\\'M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z\\'/%3E%3C/svg%3E';">
+          <img src="${product.logo}" alt="${product.name}" onerror="this.src='data:image/svg+xml,%3Csvg xmlns=\\'https://www.w3.org/2000/svg\\' viewBox=\\'0 0 24 24\\' fill=\\'%23667eea\\'%3E%3Cpath d=\\'M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z\\'/%3E%3C/svg%3E';">
         </div>
       </div>
       <div class="product-info">
